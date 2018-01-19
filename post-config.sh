d7beb2d38802ee6ab7b5a9c44bc5e4aa6ae5b823
Remon Lam <remon@containerstack.io>
2018-01-19 19:05:28 +0100

    Package management, ntp config

--- post-config.sh
+++ post-config.sh
@@ -6,8 +6,8 @@
 apt-get update
 apt-get upgrade -y
 
-## Disable swap space;
 
+## Disable swap space;
 ### Disable swap;
 swapoff -a
 
@@ -18,9 +18,20 @@ sed -i '/swap/s/^/#/g' /etc/fstab
 rm -rf /var/swap
 
 
+## Install packages;
+apt-get install -y net-tools wget nano htop iotop iftop nfs-common ntp git
 
+## Remove packages;
+apt-get remove -y command-not-found stress
+unset command_not_found_handle
 
+## NTP configuration;
+### Add ntp servers to config files;
+sed -i '$ a NTP=0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org' /etc/systemd/timesyncd.conf
+sed -i 's/debian.pool.ntp.org/nl.pool.ntp.org/g' /etc/ntp.conf
 
+## Set time zone;
+timedatectl set-timezone Europe/Amsterdam
 
 
 # Reboot node;
