5c93882146c91f26ea65ab656065d433db6d2561
Remon Lam <remon@containerstack.io>
2018-01-19 19:12:22 +0100

    enable reboot

--- post-config.sh
+++ post-config.sh
@@ -21,18 +21,23 @@ rm -rf /var/swap
 ## Install packages;
 apt-get install -y net-tools wget nano htop iotop iftop nfs-common ntp git
 
+
 ## Remove packages;
 apt-get remove -y command-not-found stress
 unset command_not_found_handle
 
-## NTP configuration;
-### Add ntp servers to config files;
-sed -i '$ a NTP=0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org' /etc/systemd/timesyncd.conf
-sed -i 's/debian.pool.ntp.org/nl.pool.ntp.org/g' /etc/ntp.conf
 
 ## Set time zone;
 timedatectl set-timezone Europe/Amsterdam
 
 
+## NTP configuration;
+### Add ntp servers to config files;
+sed -i '$ a NTP=0.nl.pool.ntp.org 1.nl.pool.ntp.org 2.nl.pool.ntp.org 3.nl.pool.ntp.org' /etc/systemd/timesyncd.conf
+sed -i 's/debian.pool.ntp.org/nl.pool.ntp.org/g' /etc/ntp.conf
+systemctl restart ntp
+timedatectl set-ntp true
+
+
 # Reboot node;
-#reboot
+reboot
